aed44d084f20a4d4bba8f55556640f8acefe64f9
Mustafa Ali <mustafamire@gmail.com>
2021-12-06 12:03:41 +0000

    Add: get auth from local storage if exists + return headers to context so httplink can read them

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,25 @@ const httpLink = createHttpLink({
   uri: "/graphql"
 });
 
+const authLink = setContext((_, {headers}) => {
+  // get the authentication token from local storage if it exists
+const token = localStorage.getItem("id_token");
+
+
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : ``
+    }
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
+
 
 function App() {
   return (
