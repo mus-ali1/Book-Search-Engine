b27290efad00108ed804d8cd243880d7f89b2147
Mustafa Ali <mustafamire@gmail.com>
2021-12-06 08:40:54 +0000

    Declare  book query with necessary mutations

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -1,3 +1,15 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+
+type Book {
+    _id: ID
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+`;
