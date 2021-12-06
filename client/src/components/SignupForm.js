653b45fe03c2205572caedeaef0e04cc13af5a59
Mustafa Ali <mustafamire@gmail.com>
2021-12-06 16:10:22 +0000

    Add: User mutation

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from "@apollo/client"
 
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 import { ADD_USER } from "../utils/mutations"
 
@@ -13,6 +12,10 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  
+  //add our mutation here
+  const [addUser, {error, data}] = useMutation(ADD_USER)
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -30,9 +33,11 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const {data} = await addUser({
+        variables: {...userFormData}
+      });
 
-      if (!response.ok) {
+      if (!data) {
         throw new Error('something went wrong!');
       }
 
